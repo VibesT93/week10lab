9a178d77093ceb17c26662a09d29ac1438745216
Violet Thomson <violetthomson93@gmail.com>
2023-03-29 19:41:54 +0100

    Fixed 99 beers

--- 99beers/script.js
+++ 99beers/script.js
@@ -4,18 +4,29 @@ function beerSong(){
 
     var beer
     var newBeer
+    var songLine
 
     for (beer=99; beer>=0; beer--){
+
         if(beer>=2){
-            System.out.println(beer+ " bottles of beer on the wall, " +beer+ " bottles of beer Take one down and pass it around,");
+
+            
             newBeer = beer -1;
-            System.out.println(newBeer +" bottles of beer on the wall");
+           
+            songLine = beer+ " bottles of beer on the wall, " +beer+ " bottles of beer Take one down and pass it around," + newBeer +" bottles of beer on the wall"
+            document.getElementById("newText").innerHTML += "<br/>" +songLine ;
             }
+
         else if(beer==1){
-            System.out.println(beer+  " bottle of beer on the wall, " +beer+ " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall");
+    
+            songLine = beer+  " bottle of beer on the wall, " +beer+ " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall"
+            document.getElementById("newText").innerHTML += "<br/>" +songLine;
             }
+
         else{
-            System.out.println("No more bottles of beer on the wall, no more bottles of beer Go to the store and buy some more, 99 bottles of beer on the wall ");
+            
+            songLine = "No more bottles of beer on the wall, no more bottles of beer Go to the store and buy some more, 99 bottles of beer on the wall "
+            document.getElementById("newText").innerHTML += "<br/>" +songLine;
             }
 
     }
