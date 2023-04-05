7fafe153bbe8d08101db18f437934aa4d7765ef9
VibesT93 <123509985+VibesT93@users.noreply.github.com>
2023-04-05 15:50:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,35 @@ function play(){
   
   for (var i = 0; i < 5; i++) {
     var min = 1;
-    var max = 3;
-    var compChoice = (Math.floor(Math.random() * (max - min + 1)) + min); // Generate a random number between 1 and 3
+    var max = 4;
+    var compChoice = (Math.floor(Math.random() * (max - min)) + min); // Generate a random number between 1 and 3
 
-    var playerChoice = prompt("Type in Paper Scissors Rock");
+    var playerChoice = prompt("Type in Rock, Paper, or Scissors");
 
     if (playerChoice == "Rock" && compChoice == 1) {
-      alert("You win");
-      results.push("Win");
+      alert("You draw");
+      results.push("Draw");
     } else if (playerChoice == "Paper" && compChoice == 2) {
-      alert("You win");
-      results.push("Win");
+      alert("You draw");
+      results.push("Draw");
     } else if (playerChoice == "Scissors" && compChoice == 3) {
+      alert("You draw");
+      results.push("Draw");
+    } else if (playerChoice == "Rock" && compChoice == 3) {
       alert("You win");
       results.push("Win");
-    } else if (playerChoice == "Rock" && compChoice == 3) {
-      alert("You draw");
-      results.push("Draw");
     } else if (playerChoice == "Paper" && compChoice == 1) {
-      alert("You draw");
-      results.push("Draw");
+      alert("You win");
+      results.push("Win");
     } else if (playerChoice == "Scissors" && compChoice == 2) {
-      alert("You draw");
-      results.push("Draw");
+      alert("You win");
+      results.push("Win");
     } else {
-      alert("You lose");
-      results.push("Loss");
+      alert("Invalid choice. Please choose Rock, Paper, or Scissors.");
+      i--; // This is so the user doesn't use up a turn with an incorrect entry
     }
   }
 
   alert("Here are the results of your 5 matches: " + results.join(", ")); // Output the array to the user
 }
+
