e23f3ccf137264f42f256229e0e9ab0c9be9d841
VibesT93 <123509985+VibesT93@users.noreply.github.com>
2023-04-05 15:08:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,41 @@
 
+function play() {
+  var results = []; // Create an empty array to store the results
+  
+  for (var i = 0; i < 5; i++) {
+    var min = Math.ceil(1);
+    var max = Math.floor(4);
+    var compChoice = Math.floor(Math.random() * (max - min) + min);
 
-function play(){
+    var playerChoice = prompt("Type in Paper Scissors Rock");
 
-    for (i=0; i<3;i++){
-
-        var min = Math.ceil(1);
-        var max =  Math.floor(4);
-        var compChoice = Math.floor(Math.random() * (max-min) + min);
-
-        let playerChoice = prompt("Type in Paper Scissors Rock");
-
-        if (playerChoice =="Rock" && compChoice == 3){
-            alert ("You win");
-            hide();
-            i = 3;
-        }
-        else if (playerChoice == "Paper" &&  compChoice == 1){
-            alert ("You win");
-            hide();
-            i = 3;
-        }
-        else if (playerChoice=="Scissors" && compChoice == 2){
-            alert ("You win");
-            hide();
-            i = 3;
-        }
-        else if (playerChoice =="Rock" && compChoice == 1){
-            alert ("You draw");
-        }
-        else if (playerChoice =="Paper" && compChoice == 2){
-            alert ("You draw");
-        }
-        else if (playerChoice =="Scissors" && compChoice == 3){
-            alert ("You draw");
-        }
-        else {
-            alert("You lose")
-            
-        }
-        hide();
-        
+    if (playerChoice == "Rock" && compChoice == 3) {
+      alert("You win");
+      results.push("Win");
+    } else if (playerChoice == "Paper" && compChoice == 1) {
+      alert("You win");
+      results.push("Win");
+    } else if (playerChoice == "Scissors" && compChoice == 2) {
+      alert("You win");
+      results.push("Win");
+    } else if (playerChoice == "Rock" && compChoice == 1) {
+      alert("You draw");
+      results.push("Draw");
+    } else if (playerChoice == "Paper" && compChoice == 2) {
+      alert("You draw");
+      results.push("Draw");
+    } else if (playerChoice == "Scissors" && compChoice == 3) {
+      alert("You draw");
+      results.push("Draw");
+    } else {
+      alert("You lose");
+      results.push("Loss");
     }
-   
+  }
+
+  alert("Results: " + results.join(", ")); // Output the array to the user as a comma-separated string
 }
 
-function hide (){
-    document.getElementById("button").style.display ="none";
+function hide() {
+  document.getElementById("button").style.display = "none";
 }
